1b6394a072120ed44b4bcf3c3abbe24ea09369c0
tbmajid <ratul.majid@gmail.com>
2022-09-24 11:48:27 +0100

    contact2

--- frontend/components/Contact.js
+++ frontend/components/Contact.js
@@ -91,111 +91,119 @@ const ContactForm = () => {
   return formSubmitted.title === "" ? (
     <div>
       {!showCaptcha ? (
-        <Grid>
-          <Card
-            style={{ maxWidth: 850, padding: "20px 5px", margin: "0 auto" }}
-          >
-            <CardContent>
-              <Typography gutterBottom variant="h5">
-                Make an enquiry
-              </Typography>
-              <Typography
-                variant="body2"
-                color="textSecondary"
-                component="p"
-                gutterBottom
-              >
-                Fill up the form and our team will get back to you within 48
-                hours.
-              </Typography>
-              <form onSubmit={submitFormAndShowCaptcha}>
-                <Grid container spacing={1}>
-                  <Grid xs={12} item>
-                    <TextField
-                      id="contact-form-name"
-                      placeholder="Enter name"
-                      label="Your Name"
-                      variant="outlined"
-                      value={name}
-                      onChange={(e) =>
-                        dispatch({ type: "name", value: e.target.value })
-                      }
-                      fullWidth
-                      required
-                    />
-                  </Grid>
-                  <Grid item xs={12}>
-                    <TextField
-                      id="contact-form-email"
-                      type="email"
-                      placeholder="Enter email"
-                      label="Email"
-                      variant="outlined"
-                      value={email}
-                      onChange={(e) =>
-                        dispatch({ type: "email", value: e.target.value })
-                      }
-                      fullWidth
-                      required
-                    />
-                  </Grid>
+        <Grid container justifyContent="center" alignItems="center">
+          <Grid item>
+            <Card
+              style={{ maxWidth: 850, padding: "20px 5px", margin: "0 auto" }}
+            >
+              <CardContent>
+                <Typography gutterBottom variant="h5">
+                  Make an enquiry
+                </Typography>
+                <Typography
+                  variant="body2"
+                  color="textSecondary"
+                  component="p"
+                  gutterBottom
+                >
+                  Fill up the form and our team will get back to you within 48
+                  hours.
+                </Typography>
+                <form onSubmit={submitFormAndShowCaptcha}>
+                  <Grid container spacing={1}>
+                    <Grid xs={12} item>
+                      <TextField
+                        id="contact-form-name"
+                        placeholder="Enter name"
+                        label="Your Name"
+                        variant="outlined"
+                        value={name}
+                        onChange={(e) =>
+                          dispatch({ type: "name", value: e.target.value })
+                        }
+                        fullWidth
+                        required
+                      />
+                    </Grid>
+                    <Grid item xs={12}>
+                      <TextField
+                        id="contact-form-email"
+                        type="email"
+                        placeholder="Enter email"
+                        label="Email"
+                        variant="outlined"
+                        value={email}
+                        onChange={(e) =>
+                          dispatch({ type: "email", value: e.target.value })
+                        }
+                        fullWidth
+                        required
+                      />
+                    </Grid>
 
-                  <Grid item xs={12}>
-                    <TextField
-                      id="contact-form-message"
-                      label="Message"
-                      multiline
-                      rows={4}
-                      placeholder="Type your message here"
-                      variant="outlined"
-                      value={message}
-                      onChange={(e) =>
-                        dispatch({ type: "message", value: e.target.value })
-                      }
-                      fullWidth
-                      required
-                    />
+                    <Grid item xs={12}>
+                      <TextField
+                        id="contact-form-message"
+                        label="Message"
+                        multiline
+                        rows={4}
+                        placeholder="Type your message here"
+                        variant="outlined"
+                        value={message}
+                        onChange={(e) =>
+                          dispatch({ type: "message", value: e.target.value })
+                        }
+                        fullWidth
+                        required
+                      />
+                    </Grid>
+                    <Grid item xs={12}>
+                      <Button
+                        type="submit"
+                        variant="contained"
+                        color="primary"
+                        fullWidth
+                      >
+                        Submit
+                      </Button>
+                    </Grid>
                   </Grid>
-                  <Grid item xs={12}>
-                    <Button
-                      type="submit"
-                      variant="contained"
-                      color="primary"
-                      fullWidth
-                    >
-                      Submit
-                    </Button>
-                  </Grid>
-                </Grid>
-              </form>{" "}
-            </CardContent>
-          </Card>
+                </form>{" "}
+              </CardContent>
+            </Card>
+          </Grid>
         </Grid>
       ) : (
-        <Grid>
-          <Card
-            style={{ maxWidth: 750, padding: "20px 5px", margin: "0 auto" }}
-          >
-            <CardContent>
-              <ReCAPTCHA
-                sitekey={process.env.NEXT_PUBLIC_CAPTCHA_SITE_KEY}
-                onChange={sendEmail}
-              />
-            </CardContent>
-          </Card>
+        <Grid container justifyContent="center" alignItems="center">
+          <Grid item>
+            <Card
+              style={{ maxWidth: 750, padding: "20px 5px", margin: "0 auto" }}
+            >
+              <CardContent>
+                <ReCAPTCHA
+                  sitekey={process.env.NEXT_PUBLIC_CAPTCHA_SITE_KEY}
+                  onChange={sendEmail}
+                />
+              </CardContent>
+            </Card>
+          </Grid>
         </Grid>
       )}
     </div>
   ) : (
     <div>
-      <Grid>
-        <Card style={{ maxWidth: 450, padding: "20px 5px", margin: "0 auto" }}>
-          <CardContent>
-            {" "}
-            <Typography>{formSubmitted.title}</Typography>
-            <p>{formSubmitted.paragraph}</p>
-          </CardContent>
-        </Card>
+      <Grid container justifyContent="center" alignItems="center">
+        <Grid item>
+          <Card
+            style={{ maxWidth: 450, padding: "20px 5px", margin: "0 auto" }}
+          >
+            <CardContent>
+              {" "}
+              <Typography>{formSubmitted.title}</Typography>
+              <p>{formSubmitted.paragraph}</p>
+            </CardContent>
+          </Card>
+        </Grid>
       </Grid>
     </div>
   );
